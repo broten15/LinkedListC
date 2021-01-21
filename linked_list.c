fe5d5306365c195884ff7f37d4c5c254fd7662c2
broten15 <brotensam@gmail.com>
2021-01-20 18:47:12 -0600

    finished LinkedList

--- linked_list.c
+++ linked_list.c
@@ -4,102 +4,166 @@
 void ll_init(struct ll_node *node) {
     assert(node != NULL);
     // TODO: Implement your solution here.
+    node->next = NULL;
+    node->prev = NULL;
 }
 
 bool ll_has_next(struct ll_node *node) {
     assert(node != NULL);
     // TODO: Implement your solution here.
-    return false;
+    return (node->next != NULL);
 }
 
 bool ll_has_prev(struct ll_node *node) {
     assert(node != NULL);
     // TODO: Implement your solution here.
-    return false;
+    return (node->prev != NULL);
 }
 
 struct ll_node *ll_next(struct ll_node *node) {
     assert(node != NULL);
     // TODO: Implement your solution here.
-    return NULL;
+    return (ll_has_next(node)) ? node->next : NULL;
 }
 
 struct ll_node *ll_prev(struct ll_node *node) {
     assert(node != NULL);
     // TODO: Implement your solution here.
-    return NULL;
+    return (ll_has_prev(node)) ? node->prev : NULL;
 }
 
 size_t ll_size(struct ll_node *head) {
     assert(head != NULL);
     // TODO: Implement your solution here.
-    return 0;
+    size_t length = 0;
+    while (head != NULL) {
+        head = ll_next(head);
+        length++;
+    }
+    return length;
 }
 
 struct ll_node *ll_head(struct ll_node *list) {
     assert(list != NULL);
     // TODO: Implement your solution here.
-    return NULL;
+    while (ll_has_prev(list)) {
+        list = ll_prev(list);
+    }
+    return list;
 }
 
 struct ll_node *ll_tail(struct ll_node *list) {
     assert(list != NULL);
     // TODO: Implement your solution here.
-    return NULL;
+    while (ll_has_next(list)) {
+        list = ll_next(list);
+    }
+    return list;
 }
 
 struct ll_node *ll_get(struct ll_node *node, size_t index) {
     assert(node != NULL);
     // TODO: Implement your solution here.
-    return NULL;
+    while (index != 0) {
+        index--;
+        node = ll_next(node);
+        if (node == NULL) {
+            return NULL;
+        }
+    }
+    return node;
 }
 
 void ll_insert_before(struct ll_node *new, struct ll_node *existing) {
     assert(new != NULL);
     assert(existing != NULL);
     // TODO: Implement your solution here.
+    if (ll_has_prev(existing)) {
+        existing->prev->next = new;
+    }
+    new->next = existing;
+    new->prev = existing->prev;
+    existing->prev = new;
 }
 
 void ll_insert_after(struct ll_node *new, struct ll_node *existing) {
     assert(new != NULL);
     assert(existing != NULL);
     // TODO: Implement your solution here.
+    if (ll_has_next(existing)) {
+        existing->next->prev = new;
+    }
+    new->next = existing->next;
+    new->prev = existing;
+    existing->next = new;
 }
 
 void ll_insert_first(struct ll_node *new, struct ll_node *list) {
     assert(new != NULL);
     assert(list != NULL);
     // TODO: Implement your solution here.
+    ll_insert_before(new, ll_head(list));
+
 }
 
 void ll_insert_last(struct ll_node *new, struct ll_node *list) {
     assert(new != NULL);
     assert(list != NULL);
     // TODO: Implement your solution here.
+    ll_insert_after(new, ll_tail(list));
 }
 
 void ll_remove(struct ll_node *node) {
     assert(node != NULL);
     // TODO: Implement your solution here.
+    if (ll_has_prev(node)) {
+        node->prev->next = node->next;
+    }
+    if (ll_has_next(node)) {
+        node->next->prev = node->prev;
+    }
 }
 
 struct ll_node *ll_min(struct ll_node *list, ll_comparator_t comparator) {
     assert(list != NULL);
     assert(comparator != NULL);
     // TODO: Implement your solution here.
-    return NULL;
+    struct ll_node *min = list;
+    while (list != NULL) {
+        if (comparator(min, list) > 0) {
+            min = list;
+        }
+        list = ll_next(list);
+    }
+    return min;
 }
 
 struct ll_node *ll_max(struct ll_node *list, ll_comparator_t comparator) {
     assert(list != NULL);
     assert(comparator != NULL);
     // TODO: Implement your solution here.
-    return NULL;
+    struct ll_node *max = list;
+    while (list != NULL) {
+        if (comparator(max, list) < 0) {
+            max = list;
+        }
+        list = ll_next(list);
+    }
+    return max;
 }
 
 struct ll_node *ll_filter(struct ll_node *list, ll_predicate_t predicate) {
     assert(list != NULL);
     assert(predicate != NULL);
     // TODO: Implement your solution here.
-    return NULL;
+    struct ll_node *new_head = NULL;
+    while (list != NULL) {
+        if (!predicate(list)) {
+            ll_remove(list);
+        } else if (new_head == NULL) {
+            new_head = list;
+        }
+        list = ll_next(list);
+    }
+    return new_head;
 }
--- student.c
+++ student.c
@@ -1,20 +1,37 @@
 #include <assert.h>
 #include "student.h"
 
+static int gpa_comparator(struct ll_node *n1, struct ll_node *n2) {
+    double gpa1 = LL_ENTRY(n1, struct student, node)->gpa;
+    double gpa2 = LL_ENTRY(n2, struct student, node)->gpa;
+    if (gpa1 < gpa2) {
+        return -1;
+    } else if (gpa2 < gpa1) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 struct student *student_worst_performer(struct student *list) {
     assert(list != NULL);
     // TODO: Implement your solution here.
-    return NULL;
+    return LL_ENTRY(ll_min(&list->node, gpa_comparator), struct student, node);
 }
 
 struct student *student_valedictorian(struct student *list) {
     assert(list != NULL);
     // TODO: Implement your solution here.
-    return NULL;
+    return LL_ENTRY(ll_max(&list->node, gpa_comparator), struct student, node);
+}
+
+static bool student_on_honor_roll(struct ll_node *n) {
+    return LL_ENTRY(n, struct student, node)->gpa >= 3.5;
 }
 
 struct student *student_honour_roll(struct student *list) {
     assert(list != NULL);
     // TODO: Implement your solution here.
-    return NULL;
+    struct ll_node *result = ll_filter(&list->node, student_on_honor_roll);
+    return (result != NULL) ? LL_ENTRY(result, struct student, node) : NULL;
 }
